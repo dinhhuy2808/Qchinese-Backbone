694452909a3d02ef25691817f58e321fcd00c9ec
huyttran <huytran.jp@interspace.vn>
2021-02-17 18:07:59 +0700

    init

---
+++ src/main/java/com/nhs3108/configs/DataSourceConfig.java
@@ -0,0 +1,42 @@
+package com.nhs3108.configs;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+/**
+ * Created by nhs3108 on 07/11/2017.
+ */
+@Configuration
+@PropertySource("classpath:application-${spring.profiles.active}.properties")
+public class DataSourceConfig {
+
+	@Value("${database.url}")
+	private String dbUrl;
+	@Value("${database.driver}")
+	private String dbDriver;
+	@Value("${database.username}")
+	private String dbUsername;
+	@Value("${database.password}")
+	private String dbPassword;
+
+    @Bean
+    public DataSource dataSource() {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.url(dbUrl);
+        dataSourceBuilder.driverClassName(dbDriver);
+        dataSourceBuilder.username(dbUsername);
+        dataSourceBuilder.password(dbPassword);
+        return dataSourceBuilder.build();
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+    	return new JdbcTemplate(dataSource());
+    }
+}
